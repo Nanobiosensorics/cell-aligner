0dd2926beec9952309325a9396ce4d08929511cc
ErikSkare <skareerik55@gmail.com>
2024-10-18 19:15:30 +0200

    fixed test after only numpy stochastic method deletion (since it is slower)

--- tests/methods_test.py
+++ tests/methods_test.py
@@ -17,18 +17,6 @@ class MethodsTest(unittest.TestCase):
     # Assert
     self.assertTrue(np.allclose(-result, translation, 0.2))
 
-  def test_find_translation_stochastic_numpy(self):
-    # Arrange
-    translation = np.array([3, 2])
-    mic_coords = np.array([[1, 0], [2, 0], [3, 0], [4, 0], [5, 0]])
-    cardio_coords = np.delete(mic_coords, [1, 2], axis=0) + translation
-
-    # Act
-    result = methods.find_translation_stochastic_numpy(cardio_coords, mic_coords, np.array([0, 1, 2]))
-
-    # Assert
-    self.assertTrue(np.allclose(-result, translation, 0.2))
-
   def test_find_translation_pmc(self):
     # Arrange
     translation = np.array([3, 2])
